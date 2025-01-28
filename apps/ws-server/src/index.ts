2d55018316fef8e7749f8fa9e1b2d37149a49b04
Arjun <arjun.ambavane@gmail.com>
2025-01-28 17:24:10 +0530

    Init

---
+++ apps/ws-server/src/index.ts
@@ -0,0 +1,36 @@
+import { verify } from "jsonwebtoken";
+import { WebSocketServer,WebSocket } from "ws";
+import {JWT_SECRET} from "@workspace/backend-common/config"
+
+const wss = new WebSocketServer({ port: 8090 });
+const checkUser = (token:string|null) => {
+    try{
+        if(!token) return null;
+        const payload = verify(token,JWT_SECRET);
+        if(typeof payload === 'string') return null;
+        if (!payload || !payload.userId) return null;
+        return payload.userId;
+    }catch(e){
+        console.log(e)
+        return null
+    }
+}
+
+wss.on('connection',(ws:WebSocket,req)=>{
+    const reqURL = req.url;
+    if(!reqURL) return;
+    const params = new URLSearchParams(reqURL.split('?')[1]);
+    const token = params.get('token');
+    const userId = checkUser(token);
+    if(!userId){
+        ws.close();
+        return;
+    }
+    ws.on('message',async (data)=>{
+        try{
+            const parsedData = JSON.parse(data as unknown as string);
+        } catch(e){
+            return null;
+        }
+    })
+})
