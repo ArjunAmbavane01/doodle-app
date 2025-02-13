645a2c8c00c49e523911eef10066e4e54d1d8178
Arjun <arjun.ambavane@gmail.com>
2025-02-13 09:05:16 +0530

    Add singleton db logic

--- packages/db/src/db.config.ts
+++ packages/db/src/db.config.ts
@@ -1,6 +1,11 @@
-import {PrismaClient} from '@prisma/client'
-// use signleton practivce here 
+import { PrismaClient } from '@prisma/client'
 
-const prisma:PrismaClient = new PrismaClient()
+const globalForPrisma = globalThis as unknown as { prisma?: PrismaClient }
+
+export const prisma = globalForPrisma.prisma ?? new PrismaClient()
+
+if (!globalForPrisma.prisma) {
+  globalForPrisma.prisma = prisma
+}
 
 export default prisma
