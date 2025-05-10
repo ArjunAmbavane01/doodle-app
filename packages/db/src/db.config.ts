6042b1e5677a72b1d052eaebd74189641a27cf5a
Arjun <arjun.ambavane@gmail.com>
2025-05-10 16:18:06 +0530

    Fix: update config files

--- packages/db/src/db.config.ts
+++ packages/db/src/db.config.ts
@@ -1,11 +1,11 @@
-import { PrismaClient } from '@prisma/client'
+import { PrismaClient } from '../dist/generated/client';
 
-const globalForPrisma = globalThis as unknown as { prisma?: PrismaClient }
+const globalForPrisma = globalThis as unknown as { prisma?: PrismaClient };
 
-export const prisma: PrismaClient = globalForPrisma.prisma ?? new PrismaClient()
+export const prisma: PrismaClient = globalForPrisma.prisma ?? new PrismaClient();
 
 if (!globalForPrisma.prisma) {
-  globalForPrisma.prisma = prisma
+  globalForPrisma.prisma = prisma;
 }
 
-export default prisma
+export default prisma;
