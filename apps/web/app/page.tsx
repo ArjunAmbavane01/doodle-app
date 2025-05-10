5bd396e10e0b73198921c287ab08a370c872532b
Arjun <arjun.ambavane@gmail.com>
2025-05-10 22:13:23 +0530

    Fix: vercel deployement issue fixed

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,8 +1,5 @@
-// import { redirect } from "next/navigation";
+import { redirect } from "next/navigation";
 
-// export default function HomeRedirect() {
-//   return redirect('/home');
-// }
-export default function Home() {
-  return <div>Home redirect reached</div>;
+export default function HomeRedirect() {
+  return redirect('/home');
 }
