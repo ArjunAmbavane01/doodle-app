d06be6f92ea9910ec4c3d7e5d99d7e47c237b611
Arjun <arjun.ambavane@gmail.com>
2025-05-11 09:19:38 +0530

    Refactor home page structure and components

--- apps/web/providers/LoadingProvider.tsx
+++ apps/web/providers/LoadingProvider.tsx
@@ -1,10 +1,10 @@
 "use client"
 import { createContext, useContext, useMemo, useState } from "react";
-import DoodleLoading from "@/app/home/_components/visuals/DoodleLoading";
+import DoodleLoading from "@/app/(home)/_components/visuals/DoodleLoading";
 
 const LoadingContext = createContext({
     isLoading: false,
-    setIsLoading: (loading: boolean) => {}
+    setIsLoading: (loading: boolean) => { }
 })
 
 export const useLoading = () => useContext(LoadingContext);
@@ -13,8 +13,8 @@ const LoadingProvider = ({ children }: { children: React.ReactNode }) => {
     const value = useMemo(() => ({ isLoading, setIsLoading }), [isLoading]);
     return (
         <LoadingContext.Provider value={value}>
-          {isLoading && <DoodleLoading />}
-          {children}
+            {isLoading && <DoodleLoading />}
+            {children}
         </LoadingContext.Provider>
     );
 }
